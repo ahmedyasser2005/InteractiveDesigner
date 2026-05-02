4d018ceecfbad6cc48855bd764a0d5e02918f39b
ahmedyasser2005 <ahmed.yasser2524@gmail.com>
2026-05-02 15:20:32 +0300

    Tiny change to wWinMain
    Added [[maybe_unused]] to wWinMain to silence the annoying compiler warnings.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 ﻿#include "Application/Application.h"
 
-int WINAPI wWinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR pCmdLine, _In_ int nCmdShow )
+int WINAPI wWinMain(
+	[[maybe_unused]] _In_ HINSTANCE hInstance,
+	[[maybe_unused]] _In_opt_ HINSTANCE hPrevInstance,
+	[[maybe_unused]] _In_ PWSTR pCmdLine,
+	[[maybe_unused]] _In_ int nCmdShow
+)
 {
 	Application app;
 	return app.Run();
