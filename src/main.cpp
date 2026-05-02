f3dd6b9fbedcbc78da4eea0b4b040d7fdee42fa8
ahmedyasser2005 <ahmed.yasser2524@gmail.com>
2026-05-02 14:18:01 +0300

    Switch entry point to wWinMain for Unicode support
    Changed the application's entry point from WinMain (ANSI) to wWinMain (Unicode), updating the function signature and parameter types to use Unicode strings. This prepares the application for better Unicode compatibility.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,6 @@
 ﻿#include "Application/Application.h"
 
-int WINAPI WinMain(
-	_In_ HINSTANCE hInstance,
-	_In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPSTR lpCmdLine,
-	_In_ int nShowCmd )
+int WINAPI wWinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR pCmdLine, _In_ int nCmdShow )
 {
 	Application app;
 	return app.Run();
